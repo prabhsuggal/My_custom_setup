d1ed1797418cc2e4383c91a7a13dce8f7767cde4
prabhsimrandeep singh <psuggal@gmail.com>
2020-11-24 11:41:40 +0530

    another small fix

--- init.sh
+++ init.sh
@@ -3,10 +3,9 @@
 while true; do
     read -p "Do you wish to install vimrc for this system?[y/n]" yn
     case $yn in
-        [Yy]* ) cd ~/;
-                git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+        [Yy]* ) git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
                 sh ~/.vim_runtime/install_awesome_vimrc.sh
-                ln -s ./vim_files/my_configs.vim ~/.vim_runtime/my_configs.vim
+                ln -s `pwd`/vim_files/my_configs.vim ~/.vim_runtime/my_configs.vim
                 break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no";;
