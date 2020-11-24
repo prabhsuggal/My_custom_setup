9040c7e12ba5ee82c95d793a0e40a68e1a16006d
prabhsimrandeep singh <psuggal@gmail.com>
2020-11-24 11:27:40 +0530

    Added chrome installation to the script

--- init.sh
+++ init.sh
@@ -4,10 +4,22 @@ while true; do
     read -p "Do you wish to install vimrc for this system?[y/n]" yn
     case $yn in
         [Yy]* ) cd ~/;
-		git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-		sh ~/.vim_runtime/install_awesome_vimrc.sh
-        ln -s ~/My_custom_setup/vim_files/my_configs.vim ~/.vim_runtime/my_configs.vim
-		break;;
+                git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+                sh ~/.vim_runtime/install_awesome_vimrc.sh
+                ln -s ~/My_custom_setup/vim_files/my_configs.vim ~/.vim_runtime/my_configs.vim
+                break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no";;
+    esac
+done
+
+while true; do
+    read -p "Do you wish to install google Chrome for this system?[y/n]" yn
+    case $yn in
+        [Yy]* ) cd ~/Downloads;
+                wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+                sudo dpkg -i google-chrome-stable_current_amd64.deb
+                break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no";;
     esac
